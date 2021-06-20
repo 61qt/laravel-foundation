bef0304105b134083b828641bdc398eebfc4e72d
xurh <xurhwork@gmail.com>
2021-06-21 01:00:42 +0800

    🐳 chore: 添加批量导入库

---
+++ src/Console/ImportTaskMakeCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace QT\Foundation\Console\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+use QT\Foundation\Traits\GeneratorModuleHelper;
+use Symfony\Component\Console\Input\InputArgument;
+
+class ImportTaskMakeCommand extends GeneratorCommand
+{
+    use GeneratorModuleHelper;
+
+    protected $name = 'make:task:import';
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return resource_path('stubs/import-task.stub');
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return "{$rootNamespace}\Tasks\Import";
+    }
+
+    /**
+     * Get the import task arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['name', InputArgument::REQUIRED, '异步任务名称.'],
+        ];
+    }
+}
