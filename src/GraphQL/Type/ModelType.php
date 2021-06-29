c69485e1e70d624e5d0042c1359057c8e2f8731a
xurh <xurhwork@gmail.com>
2021-06-29 23:58:15 +0800

    🎈 perf: 支持根据可访问字段构建object type

--- src/GraphQL/Type/ModelType.php
+++ src/GraphQL/Type/ModelType.php
@@ -2,8 +2,12 @@
 
 namespace QT\Foundation\GraphQL\Type;
 
+use Illuminate\Support\Str;
 use QT\GraphQL\GraphQLManager;
 use QT\GraphQL\Definition\Type;
+use GraphQL\Type\Definition\NonNull;
+use GraphQL\Type\Definition\ListOfType;
+use GraphQL\Type\Definition\Type as BaseType;
 use QT\GraphQL\Definition\ModelType as BaseModelType;
 
 /**
@@ -15,7 +19,7 @@ abstract class ModelType extends BaseModelType
 {
     /**
      * 允许访问的字段
-     * 
+     *
      * @var array
      */
     public $canAccess = [];
@@ -33,9 +37,65 @@ abstract class ModelType extends BaseModelType
             return $fields;
         }
 
-        // TODO 生成可访问字段
+        return $this->defineFieldMap($fields, $this->canAccess);
+    }
 
-        return $fields;
+    /**
+     * 根据允许访问的字段构建字段
+     *
+     * @param array $fields
+     * @param array $canAccess
+     * @return array
+     */
+    protected function defineFieldMap(array $fields, array $canAccess)
+    {
+        if (isset($canAccess['*'])) {
+            // 允许全部字段访问时,根据可访问字段构建
+            foreach (array_keys($fields) as $field) {
+                if (empty($canAccess[$field])) {
+                    $canAccess[$field] = true;
+                }
+            }
+        }
+
+        $results = [];
+        foreach ($canAccess as $field => $child) {
+            // 字段不存在
+            if (empty($fields[$field])) {
+                continue;
+            }
+
+            if ($fields[$field] instanceof BaseType) {
+                $fieldDef = ['type' => $fields[$field]];
+            } elseif (is_array($fields[$field]) && !empty($fields[$field]['type'])) {
+                $fieldDef = $fields[$field];
+            }
+
+            $type = $fieldDef['type'];
+            if ($type instanceof ListOfType) {
+                $type = $type->getOfType();
+            } elseif ($type instanceof NonNull) {
+                $type = $type->getOfType();
+            }
+
+            if (!is_array($child) || !$type instanceof ModelType) {
+                $results[$field] = $fieldDef;
+                continue;
+            }
+
+            // 根据配置生成可访问字段
+            $func = function () use ($type, $child) {
+                return $this->defineFieldMap(
+                    $type->getDataStructure($this->manager), $child
+                );
+            };
+
+            $results[$field] = $this->manager->create(
+                Str::camel("{$this->name}_{$field}_object"), $func
+            );
+        }
+
+        return $results;
     }
 
     /**
