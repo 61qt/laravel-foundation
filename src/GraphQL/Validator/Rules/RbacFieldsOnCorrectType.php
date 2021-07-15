d51db54b8c1210fbbb7eba70e48ac13da9a15e4a
xurh <xurhwork@gmail.com>
2021-07-15 18:53:26 +0800

    🦄 refactor: 权限不足访问type时，可以抛出正确的错误信息

--- src/GraphQL/Validator/Rules/RbacFieldsOnCorrectType.php
+++ src/GraphQL/Validator/Rules/RbacFieldsOnCorrectType.php
@@ -2,24 +2,59 @@
 
 namespace QT\Foundation\GraphQL\Validator\Rules;
 
-use GraphQL\Validator\Rules\FieldsOnCorrectType as BaseFieldsOnCorrectType;
+use GraphQL\Error\Error;
+use QT\GraphQL\GraphQLManager;
+use GraphQL\Language\AST\NodeKind;
+use GraphQL\Language\AST\FieldNode;
+use GraphQL\Validator\ValidationContext;
+use QT\Foundation\GraphQL\Definition\ModelType;
 
-class RbacFieldsOnCorrectType extends BaseFieldsOnCorrectType
+class RbacFieldsOnCorrectType
 {
+    public function __construct(protected GraphQLManager $manager)
+    {
+
+    }
+
+    public function getVisitor(ValidationContext $context)
+    {
+        return [
+            NodeKind::FIELD => function (FieldNode $node) use ($context): void {
+                $type = $context->getParentType();
+
+                if (!$type) {
+                    return;
+                }
+
+                $fieldDef = $context->getFieldDef();
+
+                if ($fieldDef) {
+                    return;
+                }
+
+                $msg = static::undefinedFieldMessage($node, $type, $this->manager);
+
+                $context->reportError(new Error($msg, [$node]));
+            },
+        ];
+    }
+
     /**
-     * @param string   $fieldName
-     * @param string   $type
-     * @param string[] $suggestedTypeNames
-     * @param string[] $suggestedFieldNames
-     *
+     * @param FieldNode $node
+     * @param string $type
+     * @param GraphQLManager $manager
      * @return string
      */
-    public static function undefinedFieldMessage(
-        $fieldName,
-        $type,
-        array $suggestedTypeNames,
-        array $suggestedFieldNames
-    ) {
-        return sprintf('没有权限访问: "%s".', $fieldName);
+    public static function undefinedFieldMessage(FieldNode $node, $type, $manager)
+    {
+        if ($type instanceof ModelType) {
+            $fields = $type->getDataStructure($manager);
+
+            if (!empty($fields[$node->name->value])) {
+                return sprintf('没有权限访问: "%s".', $node->name->value);
+            }
+        }
+
+        return sprintf('字段"%s"不存在.', $node->name->value);
     }
 }
