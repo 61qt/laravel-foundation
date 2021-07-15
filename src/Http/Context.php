c68963b8217465f22ee196526a3601b7c753830b
xurh <xurhwork@gmail.com>
2021-07-15 17:27:50 +0800

    🔧 build: 更新依赖库

--- src/Http/Context.php
+++ src/Http/Context.php
@@ -2,77 +2,9 @@
 
 namespace QT\Foundation\Http;
 
-use Illuminate\Support\Arr;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use QT\GraphQL\Contracts\Context as ContextContract;
+use QT\GraphQL\Context as BaseContext;
 
-class Context implements ContextContract
+class Context extends BaseContext
 {
-    /**
-     * Context constructor.
-     * @param Request $request
-     * @param Response $response
-     * @param array $config
-     */
-    public function __construct(
-        public Request $request,
-        public Response $response,
-        protected array $config = [],
-    ) {
-    }
 
-    /**
-     * @return Request
-     */
-    public function getRequest(): Request
-    {
-        return $this->request;
-    }
-
-    /**
-     * @return Response
-     */
-    public function getResponse(): Response
-    {
-        return $this->response;
-    }
-
-    /**
-     * @param string|int $key
-     * @param mixed $default
-     * @return mixed
-     */
-    public function setVale(string $key, mixed $value)
-    {
-        return Arr::set($this->config, $key, $value);
-    }
-
-    /**
-     * @param string|int $key
-     * @param mixed $default
-     * @return mixed
-     */
-    public function getValue(string | int $key, mixed $default = null): mixed
-    {
-        return Arr::get($this->config, $key, $default);
-    }
-
-    /**
-     * @param array $config
-     * @return void
-     */
-    public function has(string | int $key)
-    {
-        return Arr::has($this->config, $key);
-    }
-
-    /**
-     * @param array $config
-     * @return void
-     */
-    public function merge(array $config)
-    {
-        $this->config = array_merge($this->config, $config);
-    }
 }
