fae99588980a2afad309ec10812d251a01c62b66
xurh <xurhwork@gmail.com>
2022-09-26 12:21:22 +0800

    ✨ feat: 字段不存在时可以区分没权限与type不存在两种情况

--- src/GraphQL/Validator/Rules/RbacFieldsOnCorrectType.php
+++ src/GraphQL/Validator/Rules/RbacFieldsOnCorrectType.php
@@ -2,20 +2,32 @@
 
 namespace QT\Foundation\GraphQL\Validator\Rules;
 
+use Throwable;
 use GraphQL\Error\Error;
 use QT\GraphQL\GraphQLManager;
 use GraphQL\Language\AST\NodeKind;
 use GraphQL\Language\AST\FieldNode;
+use QT\Foundation\GraphQL\RbacQuery;
 use GraphQL\Validator\ValidationContext;
 use QT\Foundation\GraphQL\Definition\ModelType;
 
+/**
+ * 根据角色拥有的权限检查字段是否可用
+ * 
+ * @package QT\Foundation\GraphQL\Validator\Rules
+ */
 class RbacFieldsOnCorrectType
 {
+    /**
+     * @param GraphQLManager $manager
+     */
     public function __construct(protected GraphQLManager $manager)
     {
-
     }
 
+    /**
+     * @param ValidationContext $context
+     */
     public function getVisitor(ValidationContext $context)
     {
         return [
@@ -52,6 +64,16 @@ class RbacFieldsOnCorrectType
 
             if (!empty($fields[$node->name->value])) {
                 return sprintf('没有权限访问: "%s".', $node->name->value);
+            }
+        } if ($type instanceof RbacQuery) {
+            try {
+                // 能取回type说明type存在但是没有权限
+                $manager->getType($node->name->value);
+
+                return sprintf('没有权限访问: "%s".', $node->name->value);
+            } catch (Throwable $e) {
+                // 如果取不到type,就原样返回错误信息
+                return $e->getMessage();
             }
         }
 
